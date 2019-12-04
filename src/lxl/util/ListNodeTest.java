39571fe44d2639683b494484d5daa70a3273bd24
lxl <liuxiangli@tkpost.com>
2019-12-04 15:58:06 +0800

    12-04 测试

--- src/lxl/util/ListNodeTest.java
+++ src/lxl/util/ListNodeTest.java
@@ -48,6 +48,26 @@ public class ListNodeTest {
         return a2;
     }
 
+    public static ListNode getListNode4(){
+        ListNode a = new ListNode(1);
+        ListNode b = new ListNode(2);
+        ListNode c = new ListNode(3);
+        ListNode d = new ListNode(4);
+        ListNode e = new ListNode(5);
+        ListNode b2 = new ListNode(6);
+        ListNode c2 = new ListNode(7);
+        ListNode d2 = new ListNode(8);
+        ListNode e2 = new ListNode(9);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        e.next = b2;
+        b2.next = c2;
+        c2.next = d2;
+        d2.next = e2;
+        return a;
+    }
 
 
 }
