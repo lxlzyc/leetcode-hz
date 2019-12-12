ce9e2aaf6dc40ea2b22ae57eed19bce413fc4f53
lxl <liuxiangli@tkpost.com>
2019-12-12 10:16:48 +0800

    12-12 49. 字母异位词分组

---
+++ src/lxl/DEC/GroupAnagrams.java
@@ -0,0 +1,58 @@
+package lxl.DEC;
+
+import lxl.util.JSONUtil;
+
+import java.util.*;
+
+/**
+ * @program: leetcode-hz
+ * @description: 49. 字母异位词分组
+ * 给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。
+ *
+ * 示例:
+ *
+ * 输入: ["eat", "tea", "tan", "ate", "nat", "bat"],
+ * 输出:
+ * [
+ *   ["ate","eat","tea"],
+ *   ["nat","tan"],
+ *   ["bat"]
+ * ]
+ *
+ * 说明：
+ *
+ *     所有输入均为小写字母。
+ *     不考虑答案输出的顺序。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/group-anagrams
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author: lxl
+ * @create: 2019-12-11 18:51
+ **/
+public class GroupAnagrams {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs.length == 0) {
+            return new ArrayList();
+        }
+        Map<String, List> ans = new HashMap<String, List>();
+        for (String s : strs) {
+            char[] ca = s.toCharArray();
+            Arrays.sort(ca);
+            String key = String.valueOf(ca);
+            if (!ans.containsKey(key)){
+                ans.put(key, new ArrayList());
+            }
+            ans.get(key).add(s);
+        }
+        return new ArrayList(ans.values());
+    }
+
+    public static void main(String[] args) {
+        GroupAnagrams groupAnagrams = new GroupAnagrams();
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        System.out.println(JSONUtil.toJson(groupAnagrams.groupAnagrams(strs)));
+
+    }
+}
