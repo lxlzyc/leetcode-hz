2531cda095168ac5e16098f94fd447c669e45613
lxl <liuxiangli@tkpost.com>
2019-12-30 09:34:38 +0800

    12-27 118. 杨辉三角2

--- src/lxl/DEC/PascalsTriangle2.java
+++ src/lxl/DEC/PascalsTriangle2.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 /**
  * @program: leetcode-hz
- * @description: 118. 杨辉三角2
+ * @description: 119. 杨辉三角2
  * 给定一个非负索引 k，其中 k ≤ 33，返回杨辉三角的第 k 行。
  *
  * 在杨辉三角中，每个数是它左上方和右上方的数的和。
