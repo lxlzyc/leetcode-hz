60811006da732524c4c92643e6b08e98ca497f97
lxl <liuxiangli@tkpost.com>
2020-01-06 15:31:51 +0800

    01-06 151. 翻转字符串里的单词

---
+++ src/lxl/JAN/ReverseWordsInAString.java
@@ -0,0 +1,61 @@
+package lxl.JAN;
+
+/**
+ * @program: leetcode-hz
+ * @description: 151. 翻转字符串里的单词
+ * 给定一个字符串，逐个翻转字符串中的每个单词。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入: "the sky is blue"
+ * 输出: "blue is sky the"
+ * <p>
+ * 示例 2：
+ * <p>
+ * 输入: "  hello world!  "
+ * 输出: "world! hello"
+ * 解释: 输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
+ * <p>
+ * 示例 3：
+ * <p>
+ * 输入: "a good   example"
+ * 输出: "example good a"
+ * 解释: 如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个。
+ * <p>
+ * <p>
+ * <p>
+ * 说明：
+ * <p>
+ * 无空格字符构成一个单词。
+ * 输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/reverse-words-in-a-string
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author: lxl
+ * @create: 2020-01-06 15:21
+ **/
+public class ReverseWordsInAString {
+
+    public String reverseWords(String s) {
+        if (s.isEmpty() || s.trim().isEmpty()) {
+            return "";
+        }
+        StringBuffer stringBuffer = new StringBuffer();
+        String[] splits = s.split(" ");
+        for (int i = splits.length - 1; i >= 0; i--) {
+            if (!splits[i].isEmpty()) {
+                stringBuffer.append(" ").append(splits[i]);
+            }
+        }
+        stringBuffer.deleteCharAt(0);
+        return stringBuffer.toString();
+    }
+
+    public static void main(String[] args) {
+        ReverseWordsInAString reverseWordsInAString = new ReverseWordsInAString();
+        System.out.println(reverseWordsInAString.reverseWords("ac"));
+    }
+}
