c125ca9e8acf9897921b9991b237de341971cd73
lxl <liuxiangli@tkpost.com>
2020-01-07 14:44:35 +0800

    01-07 155.最小栈

---
+++ src/lxl/JAN/MinStack.java
@@ -0,0 +1,82 @@
+package lxl.JAN;
+
+import java.util.Stack;
+
+/**
+ * @program: leetcode-hz
+ * @description: 155. 最小栈
+ * 设计一个支持 push，pop，top 操作，并能在常数时间内检索到最小元素的栈。
+ * <p>
+ * push(x) -- 将元素 x 推入栈中。
+ * pop() -- 删除栈顶的元素。
+ * top() -- 获取栈顶元素。
+ * getMin() -- 检索栈中的最小元素。
+ * <p>
+ * 示例:
+ * <p>
+ * MinStack minStack = new MinStack();
+ * minStack.push(-2);
+ * minStack.push(0);
+ * minStack.push(-3);
+ * minStack.getMin();   --> 返回 -3.
+ * minStack.pop();
+ * minStack.top();      --> 返回 0.
+ * minStack.getMin();   --> 返回 -2.
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/min-stack
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author: lxl
+ * @create: 2020-01-07 14:22
+ **/
+public class MinStack {
+
+    private Stack<Integer> stack;
+    private Stack<Integer> min;
+
+    public MinStack() {
+        stack = new Stack<>();
+        min = new Stack<>();
+    }
+
+    public void push(int x) {
+        stack.push(x);
+        if (min.size() == 0 || min.peek() >= x) {
+            min.push(x);
+        }
+    }
+
+    public void pop() {
+        Integer x = stack.pop();
+        if (x.equals(min.peek())) {
+            min.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return min.peek();
+    }
+
+    public static void main(String[] args) {
+        MinStack minStack = new MinStack();
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+        int num = minStack.getMin();
+        System.out.println(num);
+
+        minStack.pop();
+        //System.out.println(num);
+
+        num = minStack.top();
+        System.out.println(num);
+
+        num = minStack.getMin();
+        System.out.println(num);
+
+    }
+}
