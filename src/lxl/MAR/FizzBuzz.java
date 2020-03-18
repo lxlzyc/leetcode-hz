a3ec0bf25b9391a00d2e7c7e290c6af4b8d6c28f
lxl <liuxiangli@tkpost.com>
2020-03-18 11:22:34 +0800

    03-18

---
+++ src/lxl/MAR/FizzBuzz.java
@@ -0,0 +1,69 @@
+package lxl.MAR;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @program: leetcode-hz
+ * @description: 412. Fizz Buzz
+ * 写一个程序，输出从 1 到 n 数字的字符串表示。
+ * <p>
+ * 1. 如果 n 是3的倍数，输出“Fizz”；
+ * <p>
+ * 2. 如果 n 是5的倍数，输出“Buzz”；
+ * <p>
+ * 3.如果 n 同时是3和5的倍数，输出 “FizzBuzz”。
+ * <p>
+ * 示例：
+ * <p>
+ * n = 15,
+ * <p>
+ * 返回:
+ * [
+ * "1",
+ * "2",
+ * "Fizz",
+ * "4",
+ * "Buzz",
+ * "Fizz",
+ * "7",
+ * "8",
+ * "Fizz",
+ * "Buzz",
+ * "11",
+ * "Fizz",
+ * "13",
+ * "14",
+ * "FizzBuzz"
+ * ]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/fizz-buzz
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author: lxl
+ * @create: 2020-03-18 10:08
+ **/
+public class FizzBuzz {
+    private final String FB = "FizzBuzz";
+    private final String F = "Fizz";
+    private final String B = "Buzz";
+
+    public List<String> fizzBuzz(int n) {
+        List<String> list = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0) {
+                if (i % 5 == 0) {
+                    list.add(FB);
+                } else {
+                    list.add(F);
+                }
+            } else if (i % 5 == 0) {
+                list.add(B);
+            } else {
+                list.add("" + i);
+            }
+        }
+        return list;
+    }
+
+}
