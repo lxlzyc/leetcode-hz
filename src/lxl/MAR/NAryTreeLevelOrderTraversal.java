643235c5474f16acb1f881c3f3f383ee6fddfe19
lxl <liuxiangli@tkpost.com>
2020-03-23 13:58:34 +0800

    03-23 429. N叉树的层序遍历

--- src/lxl/MAR/NAryTreeLevelOrderTraversal.java
+++ src/lxl/MAR/NAryTreeLevelOrderTraversal.java
@@ -1,7 +1,9 @@
 package lxl.MAR;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * @program: leetcode-hz
@@ -79,6 +81,24 @@ public class NAryTreeLevelOrderTraversal {
         if (!nexts.isEmpty()) {
             this.dealNodes(nexts);
         }
+    }
+
+    public List<List<Integer>> levelOrder2(Node root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                Node node = queue.poll();
+                level.add(node.val);
+                queue.addAll(node.children);
+            }
+            result.add(level);
+        }
+        return result;
     }
 
     public static void main(String[] args) {
