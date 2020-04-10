dfa69b1beaa2df1f8db97c45d7aa7df3881df84e
lxl <lxldyx000@163.com>
2020-04-10 17:13:13 +0800

    04-10 504. 七进制数

---
+++ src/lxl/APR/ConvertToBase7.java
@@ -0,0 +1,35 @@
+package lxl.APR;
+
+/**
+ * @program: leetcode-hz
+ * @description: 504. 七进制数
+ * @author: lxl
+ * @create: 2020-04-10 16:17
+ **/
+public class ConvertToBase7 {
+
+    public String convertToBase7(int num) {
+
+        StringBuilder stringBuilder = new StringBuilder();
+        boolean flag = true;
+        if (num < 0) {
+            flag = false;
+            num = 0 - num;
+        }
+        while (true) {
+
+            if (num < 7) {
+                stringBuilder.append(num);
+                break;
+            }
+            stringBuilder.append(num % 7);
+            num = num / 7;
+        }
+        return (flag ? "" : "-") + stringBuilder.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        ConvertToBase7 convertToBase7 = new ConvertToBase7();
+        System.out.println(convertToBase7.convertToBase7(-7));
+    }
+}
