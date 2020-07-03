430106fd461477ec202a47ef3e4d3a26db587e5d
lxl <lxldyx000@163.com>
2020-07-03 16:41:45 +0800

    07-03 剑指 Offer 05. 替换空格

---
+++ src/lxl/JUL/ReplaceSpace.java
@@ -0,0 +1,32 @@
+package lxl.JUL;
+
+/**
+ * @program: leetcode-hz
+ * @description: 剑指 Offer 05. 替换空格
+ * 请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：s = "We are happy."
+ * 输出："We%20are%20happy."
+ * <p>
+ * <p>
+ * <p>
+ * 限制：
+ * <p>
+ * 0 <= s 的长度 <= 10000
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/ti-huan-kong-ge-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author: lxl
+ * @create: 2020-07-03 15:58
+ **/
+public class ReplaceSpace {
+
+    public String replaceSpace(String s) {
+        return s.replace(" ", "%20");
+    }
+}
