0a0ec5aa4fa3241d500584a3a07e7b1ba7508c3e
lxl <lxldyx000@163.com>
2020-07-15 14:10:18 +0800

    07-15 96. 不同的二叉搜索树

---
+++ src/lxl/JUL/UniqueBinarySearchTrees.java
@@ -0,0 +1,51 @@
+package lxl.JUL;
+
+/**
+ * @program: leetcode-hz
+ * @description: 96. 不同的二叉搜索树
+ * 给定一个整数 n，求以 1 ... n 为节点组成的二叉搜索树有多少种？
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: 3
+ * 输出: 5
+ * 解释:
+ * 给定 n = 3, 一共有 5 种不同结构的二叉搜索树:
+ * <p>
+ * 1         3     3      2      1
+ * \       /     /      / \      \
+ * 3     2     1      1   3      2
+ * /     /       \                 \
+ * 2     1         2                 3
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/unique-binary-search-trees
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author: lxl
+ * @create: 2020-07-15 09:36
+ **/
+public class UniqueBinarySearchTrees {
+    //假设n个节点存在二叉排序树的个数是G(n)，令f(i)为以i为根的二叉搜索树的个数，则
+    //
+    //G(n)=f(1)+f(2)+f(3)+f(4)+...+f(n)
+    //
+    //当i为根节点时，其左子树节点个数为i-1个，右子树节点为n-i，则
+    //
+    //f(i)=G(i−1)∗G(n−i)
+    //
+    //作者：guanpengchn
+    //链接：https://leetcode-cn.com/problems/unique-binary-search-trees/solution/hua-jie-suan-fa-96-bu-tong-de-er-cha-sou-suo-shu-b/
+    //来源：力扣（LeetCode）
+    //著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+    public int numTrees(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i < n + 1; i++) {
+            for (int j = 1; j < i + 1; j++) {
+                dp[i] += dp[j - 1] * dp[i - j];
+            }
+        }
+        return dp[n];
+    }
+}
