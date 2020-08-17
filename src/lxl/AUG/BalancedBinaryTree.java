60365a8e27c2c3f6287154cec73bf21372de6d09
lxl <lxldyx000@163.com>
2020-08-17 14:52:05 +0800

    08-17 110. 平衡二叉树

---
+++ src/lxl/AUG/BalancedBinaryTree.java
@@ -0,0 +1,94 @@
+package lxl.AUG;
+
+/**
+ * @program: leetcode-hz
+ * @description: 110. 平衡二叉树
+ * 给定一个二叉树，判断它是否是高度平衡的二叉树。
+ * <p>
+ * 本题中，一棵高度平衡二叉树定义为：
+ * <p>
+ * 一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过1。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 给定二叉树 [3,9,20,null,null,15,7]
+ * <p>
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * <p>
+ * 返回 true 。
+ * <p>
+ * 示例 2:
+ * <p>
+ * 给定二叉树 [1,2,2,3,3,null,null,4,4]
+ * <p>
+ * 1
+ * / \
+ * 2   2
+ * / \
+ * 3   3
+ * / \
+ * 4   4
+ * <p>
+ * 返回 false 。
+ * <p>
+ * <p>
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/balanced-binary-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author: lxl
+ * @create: 2020-08-17 09:43
+ **/
+public class BalancedBinaryTree {
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public boolean isBalanced(TreeNode root) {
+        return height(root) >= 0;
+    }
+
+    public int height(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        if (leftHeight == -1 || rightHeight == -1 || Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        } else {
+            return Math.max(leftHeight, rightHeight) + 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        BalancedBinaryTree balancedBinaryTree = new BalancedBinaryTree();
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node21 = new TreeNode(2);
+        TreeNode node22 = new TreeNode(2);
+        TreeNode node31 = new TreeNode(3);
+        TreeNode node32 = new TreeNode(3);
+        TreeNode node41 = new TreeNode(4);
+        TreeNode node42 = new TreeNode(4);
+
+        node1.left = node21;
+        node1.right = node22;
+        node21.left = node31;
+        node21.right = node32;
+        node31.left = node41;
+        node31.right = node42;
+        System.out.println(balancedBinaryTree.isBalanced(node1));
+
+    }
+}
