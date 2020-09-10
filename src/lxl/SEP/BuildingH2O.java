03602fe218a6c79220037e441e0aeddbac0fa034
lxl <lxldyx000@163.com>
2020-09-10 16:36:34 +0800

    09-10 1117. H2O 生成

---
+++ src/lxl/SEP/BuildingH2O.java
@@ -0,0 +1,133 @@
+package lxl.SEP;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Semaphore;
+
+/**
+ * @program: leetcode-hz
+ * @description: 1117. H2O 生成
+ * 现在有两种线程，氧 oxygen 和氢 hydrogen，你的目标是组织这两种线程来产生水分子。
+ * <p>
+ * 存在一个屏障（barrier）使得每个线程必须等候直到一个完整水分子能够被产生出来。
+ * <p>
+ * 氢和氧线程会被分别给予 releaseHydrogen 和 releaseOxygen 方法来允许它们突破屏障。
+ * <p>
+ * 这些线程应该三三成组突破屏障并能立即组合产生一个水分子。
+ * <p>
+ * 你必须保证产生一个水分子所需线程的结合必须发生在下一个水分子产生之前。
+ * <p>
+ * 换句话说:
+ * <p>
+ * 如果一个氧线程到达屏障时没有氢线程到达，它必须等候直到两个氢线程到达。
+ * 如果一个氢线程到达屏障时没有其它线程到达，它必须等候直到一个氧线程和另一个氢线程到达。
+ * <p>
+ * 书写满足这些限制条件的氢、氧线程同步代码。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: "HOH"
+ * 输出: "HHO"
+ * 解释: "HOH" 和 "OHH" 依然都是有效解。
+ * <p>
+ * 示例 2:
+ * <p>
+ * 输入: "OOHHHH"
+ * 输出: "HHOHHO"
+ * 解释: "HOHHHO", "OHHHHO", "HHOHOH", "HOHHOH", "OHHHOH", "HHOOHH", "HOHOHH" 和 "OHHOHH" 依然都是有效解。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/building-h2o
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author: lxl
+ * @create: 2020-09-10 15:27
+ **/
+public class BuildingH2O {
+
+    private CyclicBarrier cyclicBarrier;
+    private Semaphore o;
+    private Semaphore h;
+
+    public BuildingH2O() {
+        o = new Semaphore(1);
+        h = new Semaphore(2);
+        cyclicBarrier = new CyclicBarrier(3, new Runnable() {
+            @Override
+            public void run() {
+                o.release(1);
+                h.release(2);
+            }
+        });
+    }
+
+    public void hydrogen(Runnable releaseHydrogen) throws InterruptedException {
+        h.acquire();
+        // releaseHydrogen.run() outputs "H". Do not change or remove this line.
+        releaseHydrogen.run();
+        try {
+            cyclicBarrier.await();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void oxygen(Runnable releaseOxygen) throws InterruptedException {
+        o.acquire();
+        // releaseOxygen.run() outputs "O". Do not change or remove this line.
+        releaseOxygen.run();
+        try {
+            cyclicBarrier.await();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Runnable releaseHydrogen = new Runnable() {
+            @Override
+            public void run() {
+                System.out.print('H');
+            }
+        };
+        Runnable releaseOxygen = new Runnable() {
+            @Override
+            public void run() {
+                System.out.print('O');
+            }
+        };
+        BuildingH2O buildingH2O = new BuildingH2O();
+        int n = 50;
+        Runnable runnable1 = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    for (int i = 0; i < n; i++) {
+                        buildingH2O.hydrogen(releaseHydrogen);
+
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        Runnable runnable2 = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    for (int i = 0; i < n; i++) {
+                        buildingH2O.oxygen(releaseOxygen);
+
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        new Thread(runnable1).start();
+        new Thread(runnable2).start();
+    }
+
+}
