518d55aa45ea64e51395b9f9b420822e4e971623
lxl <lxldyx000@163.com>
2020-10-12 14:24:15 +0800

    10-12 518. 零钱兑换 II

---
+++ src/lxl/OCT/CoinChange2.java
@@ -0,0 +1,68 @@
+package lxl.OCT;
+
+/**
+ * @program: leetcode-hz
+ * @description: 518. 零钱兑换 II
+ * 给定不同面额的硬币和一个总金额。写出函数来计算可以凑成总金额的硬币组合数。假设每一种面额的硬币有无限个。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: amount = 5, coins = [1, 2, 5]
+ * 输出: 4
+ * 解释: 有四种方式可以凑成总金额:
+ * 5=5
+ * 5=2+2+1
+ * 5=2+1+1+1
+ * 5=1+1+1+1+1
+ * <p>
+ * 示例 2:
+ * <p>
+ * 输入: amount = 3, coins = [2]
+ * 输出: 0
+ * 解释: 只用面额2的硬币不能凑成总金额3。
+ * <p>
+ * 示例 3:
+ * <p>
+ * 输入: amount = 10, coins = [10]
+ * 输出: 1
+ * <p>
+ * <p>
+ * <p>
+ * 注意:
+ * <p>
+ * 你可以假设：
+ * <p>
+ * 0 <= amount (总金额) <= 5000
+ * 1 <= coin (硬币面额) <= 5000
+ * 硬币种类不超过 500 种
+ * 结果符合 32 位符号整数
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/coin-change-2
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author: lxl
+ * @create: 2020-10-12 13:45
+ **/
+public class CoinChange2 {
+
+    public int change(int amount, int[] coins) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for (int coin : coins) {
+            for (int x = coin; x < amount + 1; ++x) {
+                dp[x] += dp[x - coin];
+            }
+        }
+        return dp[amount];
+    }
+
+    public static void main(String[] args) {
+        CoinChange2 coinChange2 = new CoinChange2();
+        int[] coins = {1, 2, 5};
+        System.out.println(coinChange2.change(5, coins));
+    }
+
+
+}
