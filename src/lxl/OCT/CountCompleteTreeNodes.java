30db7a555c8cd9dc90448620e545625046149e34
lxl <lxldyx000@163.com>
2020-10-13 15:33:57 +0800

    10-13 222. 完全二叉树的节点个数

---
+++ src/lxl/OCT/CountCompleteTreeNodes.java
@@ -0,0 +1,42 @@
+package lxl.OCT;
+
+import lxl.util.TreeNode;
+
+/**
+ * @program: leetcode-hz
+ * @description: 222. 完全二叉树的节点个数
+ * 给出一个完全二叉树，求出该树的节点个数。
+ * <p>
+ * 说明：
+ * <p>
+ * 完全二叉树的定义如下：在完全二叉树中，除了最底层节点可能没填满外，其余每层节点数都达到最大值，并且最下面一层的节点都集中在该层最左边的若干位置。
+ * 若最底层为第 h 层，则该层包含 1~ 2h 个节点。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入:
+ * 1
+ * / \
+ * 2   3
+ * / \  /
+ * 4  5 6
+ * <p>
+ * 输出: 6
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/count-complete-tree-nodes
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author: lxl
+ * @create: 2020-10-13 09:55
+ **/
+public class CountCompleteTreeNodes {
+
+    public int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+
+
+}
