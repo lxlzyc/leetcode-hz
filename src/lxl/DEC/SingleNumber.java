f1ceea9f69a9cc1bbce0da5a9e38c84c761ef907
lxl <lxldyx000@163.com>
2020-10-19 10:59:54 +0800

    10-19 添加测试

--- src/lxl/DEC/SingleNumber.java
+++ src/lxl/DEC/SingleNumber.java
@@ -37,4 +37,9 @@ public class SingleNumber {
         return ans;
     }
 
+    public static void main(String[] args) {
+        System.out.println(1 ^ 1);
+        System.out.println(1 ^ 0);
+    }
+
 }
