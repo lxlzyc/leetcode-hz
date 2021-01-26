3f41b38e28b33fe6052f4396eb95cae8067111bb
liuxiangli <liuxiangli@hbh.ltd>
2021-01-26 10:51:25 +0800

    2021-01-26 380. 常数时间插入、删除和获取随机元素

---
+++ src/lxl/y2021/JAN/InsertDeleteGetrandomO1.java
@@ -0,0 +1,100 @@
+package lxl.y2021.JAN;
+
+import java.util.*;
+
+/**
+ * @author lxl
+ * @program leetcode-hz
+ * @description: 380. 常数时间插入、删除和获取随机元素
+ * 设计一个支持在平均 时间复杂度 O(1) 下，执行以下操作的数据结构。
+ * <p>
+ * insert(val)：当元素 val 不存在时，向集合中插入该项。
+ * remove(val)：元素 val 存在时，从集合中移除该项。
+ * getRandom：随机返回现有集合中的一项。每个元素应该有相同的概率被返回。
+ * <p>
+ * 示例 :
+ * <p>
+ * // 初始化一个空的集合。
+ * RandomizedSet randomSet = new RandomizedSet();
+ * <p>
+ * // 向集合中插入 1 。返回 true 表示 1 被成功地插入。
+ * randomSet.insert(1);
+ * <p>
+ * // 返回 false ，表示集合中不存在 2 。
+ * randomSet.remove(2);
+ * <p>
+ * // 向集合中插入 2 。返回 true 。集合现在包含 [1,2] 。
+ * randomSet.insert(2);
+ * <p>
+ * // getRandom 应随机返回 1 或 2 。
+ * randomSet.getRandom();
+ * <p>
+ * // 从集合中移除 1 ，返回 true 。集合现在包含 [2] 。
+ * randomSet.remove(1);
+ * <p>
+ * // 2 已在集合中，所以返回 false 。
+ * randomSet.insert(2);
+ * <p>
+ * // 由于 2 是集合中唯一的数字，getRandom 总是返回 2 。
+ * randomSet.getRandom();
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/insert-delete-getrandom-o1
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @date 2021/1/26 10:09
+ * @Version 1.0
+ */
+public class InsertDeleteGetrandomO1 {
+    private ArrayList<Integer> list;
+    private Map<Integer, Integer> values;
+    private Random random;
+
+    /**
+     * Initialize your data structure here.
+     */
+    public InsertDeleteGetrandomO1() {
+        list = new ArrayList<>();
+        values = new HashMap<>();
+        random = new Random();
+    }
+
+    /**
+     * Inserts a value to the set. Returns true if the set did not already contain the specified element.
+     */
+    public boolean insert(int val) {
+        if (values.containsKey(val)) {
+            return false;
+        }
+        values.put(val, list.size());
+        list.add(list.size(), val);
+        return true;
+    }
+
+    /**
+     * Removes a value from the set. Returns true if the set contained the specified element.
+     */
+    public boolean remove(int val) {
+        if (values.containsKey(val)) {
+            // move the last element to the place idx of the element to delete
+            int lastElement = list.get(list.size() - 1);
+            int idx = values.get(val);
+            list.set(idx, lastElement);
+            values.put(lastElement, idx);
+            // delete the last element
+            list.remove(list.size() - 1);
+            values.remove(val);
+
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Get a random element from the set.
+     */
+    public int getRandom() {
+        return list.get(random.nextInt(list.size()));
+    }
+}
+
+}
