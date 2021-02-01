c82273d5b17473e6b8fa82eba8b512d0198cfffb
liuxiangli <liuxiangli@hbh.ltd>
2021-02-01 15:01:31 +0800

    2021-02-01 剑指 Offer 59 - I. 滑动窗口的最大值

---
+++ src/lxl/y2021/FEB/MaxSlidingWindow.java
@@ -0,0 +1,92 @@
+package lxl.y2021.FEB;
+
+import java.util.*;
+
+/**
+ * @author lxl
+ * @program leetcode-hz
+ * @description: 剑指 Offer 59 - I. 滑动窗口的最大值
+ * 给定一个数组 nums 和滑动窗口的大小 k，请找出所有滑动窗口里的最大值。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: nums = [1,3,-1,-3,5,3,6,7], 和 k = 3
+ * 输出: [3,3,5,5,6,7]
+ * 解释:
+ * <p>
+ * 滑动窗口的位置                最大值
+ * ---------------               -----
+ * [1  3  -1] -3  5  3  6  7       3
+ * 1 [3  -1  -3] 5  3  6  7       3
+ * 1  3 [-1  -3  5] 3  6  7       5
+ * 1  3  -1 [-3  5  3] 6  7       5
+ * 1  3  -1  -3 [5  3  6] 7       6
+ * 1  3  -1  -3  5 [3  6  7]      7
+ * <p>
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 你可以假设 k 总是有效的，在输入数组不为空的情况下，1 ≤ k ≤ 输入数组的大小。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/hua-dong-chuang-kou-de-zui-da-zhi-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @date 2021/2/1 14:22
+ * @Version 1.0
+ */
+public class MaxSlidingWindow {
+
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int len = nums.length;
+        if (len <= 1) {
+            return nums;
+        }
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return Integer.compare(o2, o1);
+            }
+        });
+        int[] ans = new int[len - k + 1];
+        for (int i = 0; i < k; i++) {
+            priorityQueue.offer(nums[i]);
+        }
+        ans[0] = priorityQueue.peek();
+        for (int i = 1; i < len - k + 1; i++) {
+            priorityQueue.remove(nums[i - 1]);
+            priorityQueue.offer(nums[i + k - 1]);
+            ans[i] = priorityQueue.peek();
+        }
+        return ans;
+    }
+
+    //单调栈
+    public int[] maxSlidingWindow2(int[] nums, int k) {
+        if (nums.length == 0 || k == 0) return new int[0];
+        Deque<Integer> deque = new LinkedList<>();
+        int[] res = new int[nums.length - k + 1];
+        for (int i = 0; i < k; i++) { // 未形成窗口
+            while (!deque.isEmpty() && deque.peekLast() < nums[i])
+                deque.removeLast();
+            deque.addLast(nums[i]);
+        }
+        res[0] = deque.peekFirst();
+        for (int i = k; i < nums.length; i++) { // 形成窗口后
+            if (deque.peekFirst() == nums[i - k])
+                deque.removeFirst();
+            while (!deque.isEmpty() && deque.peekLast() < nums[i])
+                deque.removeLast();
+            deque.addLast(nums[i]);
+            res[i - k + 1] = deque.peekFirst();
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        MaxSlidingWindow maxSlidingWindow = new MaxSlidingWindow();
+        int[] nums = {1, 3, -1, -3, 5, 3, 6, 7};
+        int k = 3;
+        System.out.println(Arrays.toString(maxSlidingWindow.maxSlidingWindow(nums, k)));
+    }
+}
