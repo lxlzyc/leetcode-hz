0d93c85e7ff279e45908bd3c2d88eadefcd93018
liuxiangli <liuxiangli@hbh.ltd>
2021-02-01 15:01:50 +0800

    2021-02-01 剑指 Offer 58 - I. 翻转单词顺序

---
+++ src/lxl/y2021/FEB/ReverseWords.java
@@ -0,0 +1,53 @@
+package lxl.y2021.FEB;
+
+/**
+ * @author lxl
+ * @program leetcode-hz
+ * @description: 剑指 Offer 58 - I. 翻转单词顺序
+ * 输入一个英文句子，翻转句子中单词的顺序，但单词内字符的顺序不变。为简单起见，标点符号和普通字母一样处理。例如输入字符串"I am a student. "，则输出"student. a am I"。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入: "the sky is blue"
+ * 输出: "blue is sky the"
+ * <p>
+ * 示例 2：
+ * <p>
+ * 输入: "  hello world!  "
+ * 输出: "world! hello"
+ * 解释: 输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
+ * <p>
+ * 示例 3：
+ * <p>
+ * 输入: "a good   example"
+ * 输出: "example good a"
+ * 解释: 如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个。
+ * <p>
+ * <p>
+ * <p>
+ * 说明：
+ * <p>
+ * 无空格字符构成一个单词。
+ * 输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
+ * 如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/fan-zhuan-dan-ci-shun-xu-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @date 2021/2/1 14:17
+ * @Version 1.0
+ */
+public class ReverseWords {
+    public String reverseWords(String s) {
+        String[] help = s.split(" ");
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = help.length - 1; i >= 0; i--) {
+            if (!help[i].trim().isEmpty()) {
+                stringBuilder.append(help[i]).append(" ");
+            }
+        }
+        return stringBuilder.toString().trim();
+    }
+}
