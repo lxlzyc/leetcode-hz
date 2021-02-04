b0c66e37473ca16ee0ebf07fde1a5fa6a6172f11
liuxiangli <liuxiangli@hbh.ltd>
2021-02-04 10:44:48 +0800

    2021-02-04 643. 子数组最大平均数 I

---
+++ src/lxl/y2021/FEB/MaximumAverageSubarrayi.java
@@ -0,0 +1,44 @@
+package lxl.y2021.FEB;
+
+/**
+ * @author lxl
+ * @program leetcode-hz
+ * @description: 643. 子数组最大平均数 I
+ * 给定 n 个整数，找出平均数最大且长度为 k 的连续子数组，并输出该最大平均数。
+ * <p>
+ * <p>
+ * <p>
+ * 示例：
+ * <p>
+ * 输入：[1,12,-5,-6,50,3], k = 4
+ * 输出：12.75
+ * 解释：最大平均数 (12-5-6+50)/4 = 51/4 = 12.75
+ * <p>
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= k <= n <= 30,000。
+ * 所给数据范围 [-10,000，10,000]。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/maximum-average-subarray-i
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @date 2021/2/4 9:49
+ * @Version 1.0
+ */
+public class MaximumAverageSubarrayi {
+    public double findMaxAverage(int[] nums, int k) {
+        int sum = 0;
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
+        }
+        double max = sum;
+        for (int i = k, l = nums.length; i < l; i++) {
+            sum += nums[i];
+            sum -= nums[i - k];
+            max = Math.max(sum, max);
+        }
+        return max / k;
+    }
+}
