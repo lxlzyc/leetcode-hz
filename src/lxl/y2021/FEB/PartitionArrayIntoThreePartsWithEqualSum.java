a28428a6d75c82e55ad424487103c835c507eb45
liuxiangli <liuxiangli@hbh.ltd>
2021-02-24 11:11:00 +0800

    2021-02-24 1013. 将数组分成和相等的三个部分

---
+++ src/lxl/y2021/FEB/PartitionArrayIntoThreePartsWithEqualSum.java
@@ -0,0 +1,72 @@
+package lxl.y2021.FEB;
+
+/**
+ * @author lxl
+ * @program leetcode-hz
+ * @description: 1013. 将数组分成和相等的三个部分
+ * 给你一个整数数组 A，只有可以将其划分为三个和相等的非空部分时才返回 true，否则返回 false。
+ * <p>
+ * 形式上，如果可以找出索引 i+1 < j 且满足 A[0] + A[1] + ... + A[i] == A[i+1] + A[i+2] + ... + A[j-1] == A[j] + A[j-1] + ... + A[A.length - 1] 就可以将数组三等分。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：[0,2,1,-6,6,-7,9,1,2,0,1]
+ * 输出：true
+ * 解释：0 + 2 + 1 = -6 + 6 - 7 + 9 + 1 = 2 + 0 + 1
+ * <p>
+ * 示例 2：
+ * <p>
+ * 输入：[0,2,1,-6,6,7,9,-1,2,0,1]
+ * 输出：false
+ * <p>
+ * 示例 3：
+ * <p>
+ * 输入：[3,3,6,5,-2,2,5,1,-9,4]
+ * 输出：true
+ * 解释：3 + 3 = 6 = 5 - 2 + 2 + 5 + 1 - 9 + 4
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/partition-array-into-three-parts-with-equal-sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @date 2021/2/23 10:19
+ * @Version 1.0
+ */
+public class PartitionArrayIntoThreePartsWithEqualSum {
+    public boolean canThreePartsEqualSum(int[] arr) {
+        int sum = 0;
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+        }
+        if (sum % 3 != 0) {
+            return false;
+        }
+        int part = sum / 3;
+        int offset = 0;
+        int help = 0;
+        while (offset < n - 2) {
+            help += arr[offset];
+            if (help == part) {
+                help = 0;
+                offset++;
+                while (offset < n - 1) {
+                    help += arr[offset];
+                    if (help == part) {
+                        return true;
+                    }
+                    offset++;
+                }
+            }
+            offset++;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        PartitionArrayIntoThreePartsWithEqualSum partitionArrayIntoThreePartsWithEqualSum = new PartitionArrayIntoThreePartsWithEqualSum();
+        int[] arr = {0, 2, 1, -6, 6, 7, 9, -1, 2, 0, 1};
+        System.out.println(partitionArrayIntoThreePartsWithEqualSum.canThreePartsEqualSum(arr));
+    }
+}
