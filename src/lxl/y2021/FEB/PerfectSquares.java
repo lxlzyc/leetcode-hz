cc5df0bb1c465466fdb1e1f6336d22def178af6a
liuxiangli <liuxiangli@hbh.ltd>
2021-02-27 10:42:02 +0800

    2021-02-27 279. 完全平方数

---
+++ src/lxl/y2021/FEB/PerfectSquares.java
@@ -0,0 +1,62 @@
+package lxl.y2021.FEB;
+
+import java.util.Arrays;
+
+/**
+ * @author lxl
+ * @program leetcode-hz
+ * @description: 279. 完全平方数
+ * 给定正整数 n，找到若干个完全平方数（比如 1, 4, 9, 16, ...）使得它们的和等于 n。你需要让组成和的完全平方数的个数最少。
+ * <p>
+ * 给你一个整数 n ，返回和为 n 的完全平方数的 最少数量 。
+ * <p>
+ * 完全平方数 是一个整数，其值等于另一个整数的平方；换句话说，其值等于一个整数自乘的积。例如，1、4、9 和 16 都是完全平方数，而 3 和 11 不是。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：n = 12
+ * 输出：3
+ * 解释：12 = 4 + 4 + 4
+ * <p>
+ * 示例 2：
+ * <p>
+ * 输入：n = 13
+ * 输出：2
+ * 解释：13 = 4 + 9
+ * <p>
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= n <= 104
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/perfect-squares
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @date 2021/2/27 9:58
+ * @Version 1.0
+ */
+public class PerfectSquares {
+    //dp伪暴力
+    public int numSquares(int n) {
+        int dp[] = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        int max_square_index = (int) Math.sqrt(n) + 1;
+        int square_nums[] = new int[max_square_index];
+        for (int i = 1; i < max_square_index; ++i) {
+            square_nums[i] = i * i;
+        }
+        for (int i = 1; i <= n; ++i) {
+            for (int s = 1; s < max_square_index; ++s) {
+                if (i < square_nums[s]) {
+                    break;
+                }
+                dp[i] = Math.min(dp[i], dp[i - square_nums[s]] + 1);
+            }
+        }
+        return dp[n];
+    }
+}
