5ca1c087f014ebff827bf7c37b182fb7dad3b6b5
liuxiangli <liuxiangli@hbh.ltd>
2021-02-27 10:42:16 +0800

    2021-02-27 69. x 的平方根

---
+++ src/lxl/y2021/FEB/Sqrtx.java
@@ -0,0 +1,47 @@
+package lxl.y2021.FEB;
+
+/**
+ * @author lxl
+ * @program leetcode-hz
+ * @description: 69. x 的平方根
+ * 实现 int sqrt(int x) 函数。
+ * <p>
+ * 计算并返回 x 的平方根，其中 x 是非负整数。
+ * <p>
+ * 由于返回类型是整数，结果只保留整数的部分，小数部分将被舍去。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: 4
+ * 输出: 2
+ * <p>
+ * 示例 2:
+ * <p>
+ * 输入: 8
+ * 输出: 2
+ * 说明: 8 的平方根是 2.82842...,
+ * 由于返回类型是整数，小数部分将被舍去。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/sqrtx
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @date 2021/2/27 10:27
+ * @Version 1.0
+ */
+public class Sqrtx {
+    public int mySqrt(int x) {
+        int left = 0;
+        int right = x;
+        int ans = -1;
+        while (left <= x) {
+            int mid = left + (right - left) / 2;
+            if ((long) mid * mid <= x) {
+                ans = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return ans;
+    }
+}
