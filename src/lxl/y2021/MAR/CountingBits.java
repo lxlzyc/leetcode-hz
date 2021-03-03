dfc80d4e459fd1c42545edcd03aa0ee150b18e2c
liuxiangli <liuxiangli@hbh.ltd>
2021-03-03 15:13:34 +0800

    2021-03-03 338. 比特位计数

--- src/lxl/y2021/MAR/CountingBits.java
+++ src/lxl/y2021/MAR/CountingBits.java
@@ -29,14 +29,28 @@ package lxl.y2021.MAR;
  * @Version 1.0
  */
 public class CountingBits {
+    //    对于所有的数字，只有两类：
+//    奇数：二进制表示中，奇数一定比前面那个偶数多一个 1，因为多的就是最低位的 1。
+//    举例：
+//            0 = 0       1 = 1
+//            2 = 10      3 = 11
+//    偶数：二进制表示中，偶数中 1 的个数一定和除以 2 之后的那个数一样多。因为最低位是 0，除以 2 就是右移一位，
+//         也就是把那个 0 抹掉而已，所以 1 的个数是不变的。
+//    另外，0 的 1 个数为 0，于是就可以根据奇偶性开始遍历计算了。
+//
+//    作者：duadua
+//    链接：https://leetcode-cn.com/problems/counting-bits/solution/hen-qing-xi-de-si-lu-by-duadua/
+//    来源：力扣（LeetCode）
+//    著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
     public int[] countBits(int num) {
         int[] bits = new int[num + 1];
         int highBit = 0;
         for (int i = 1; i <= num; i++) {
-            if ((i & (i - 1)) == 0) {
-                highBit = i;
+            if (i % 2 == 0) {
+                bits[i] = bits[i / 2];
+            } else {
+                bits[i] = bits[i - 1] + 1;
             }
-            bits[i] = bits[i - highBit] + 1;
         }
         return bits;
     }
