7c1a4acb0177ba09b529ddc3616465d7cc57fe79
liuxiangli <liuxiangli@hbh.ltd>
2021-03-07 16:51:02 +0800

    2021-03-07 readme

---
+++ src/lxl/y2021/MAR/NonDecreasingArray.java
@@ -0,0 +1,55 @@
+package lxl.y2021.MAR;
+
+/**
+ * @author lxl
+ * @program leetcode-hz
+ * @description: 665. 非递减数列
+ * 给你一个长度为 n 的整数数组，请你判断在 最多 改变 1 个元素的情况下，该数组能否变成一个非递减数列。
+ * <p>
+ * 我们是这样定义一个非递减数列的： 对于数组中任意的 i (0 <= i <= n-2)，总满足 nums[i] <= nums[i + 1]。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: nums = [4,2,3]
+ * 输出: true
+ * 解释: 你可以通过把第一个4变成1来使得它成为一个非递减数列。
+ * <p>
+ * 示例 2:
+ * <p>
+ * 输入: nums = [4,2,1]
+ * 输出: false
+ * 解释: 你不能在只改变一个元素的情况下将其变为非递减数列。
+ * <p>
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= n <= 10 ^ 4
+ * - 10 ^ 5 <= nums[i] <= 10 ^ 5
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/non-decreasing-array
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @date 2021/3/7 16:46
+ * @Version 1.0
+ */
+public class NonDecreasingArray {
+    public boolean checkPossibility(int[] nums) {
+        int n = nums.length, cnt = 0;
+        for (int i = 0; i < n - 1; ++i) {
+            int x = nums[i], y = nums[i + 1];
+            if (x > y) {
+                cnt++;
+                if (cnt > 1) {
+                    return false;
+                }
+                if (i > 0 && y < nums[i - 1]) {
+                    nums[i + 1] = x;
+                }
+            }
+        }
+        return true;
+    }
+}
