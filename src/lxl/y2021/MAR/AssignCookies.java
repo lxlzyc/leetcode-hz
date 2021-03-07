a0117e091538d17c60b2dfa78c73a74ff6c84d7c
liuxiangli <liuxiangli@hbh.ltd>
2021-03-07 14:03:09 +0800

    2021-03-06 455. 分发饼干

---
+++ src/lxl/y2021/MAR/AssignCookies.java
@@ -0,0 +1,70 @@
+package lxl.y2021.MAR;
+
+import java.util.Arrays;
+
+/**
+ * @author lxl
+ * @program leetcode-hz
+ * @description: 455. 分发饼干
+ * 假设你是一位很棒的家长，想要给你的孩子们一些小饼干。但是，每个孩子最多只能给一块饼干。
+ * <p>
+ * 对每个孩子 i，都有一个胃口值 g[i]，这是能让孩子们满足胃口的饼干的最小尺寸；并且每块饼干 j，都有一个尺寸 s[j] 。如果 s[j] >= g[i]，我们可以将这个饼干 j 分配给孩子 i ，这个孩子会得到满足。你的目标是尽可能满足越多数量的孩子，并输出这个最大数值。
+ * <p>
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: g = [1,2,3], s = [1,1]
+ * 输出: 1
+ * 解释:
+ * 你有三个孩子和两块小饼干，3个孩子的胃口值分别是：1,2,3。
+ * 虽然你有两块小饼干，由于他们的尺寸都是1，你只能让胃口值是1的孩子满足。
+ * 所以你应该输出1。
+ * <p>
+ * 示例 2:
+ * <p>
+ * 输入: g = [1,2], s = [1,2,3]
+ * 输出: 2
+ * 解释:
+ * 你有两个孩子和三块小饼干，2个孩子的胃口值分别是1,2。
+ * 你拥有的饼干数量和尺寸都足以让所有孩子满足。
+ * 所以你应该输出2.
+ * <p>
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= g.length <= 3 * 104
+ * 0 <= s.length <= 3 * 104
+ * 1 <= g[i], s[j] <= 231 - 1
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/assign-cookies
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @date 2021/3/6 15:35
+ * @Version 1.0
+ */
+public class AssignCookies {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int sright = s.length - 1;
+        int sum = 0;
+        int gright = g.length - 1;
+        while (sright >= 0) {
+            while (gright >= 0) {
+                if (s[sright] < g[gright]) {
+                    gright--;
+                } else {
+                    sum++;
+                    gright--;
+                    sright--;
+                    break;
+                }
+            }
+            if (gright < 0) {
+                break;
+            }
+        }
+        return sum;
+    }
+}
