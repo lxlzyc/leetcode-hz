99b6aade6f011bd8bc43f2fd3dbf2711a4bdab9e
liuxiangli <liuxiangli@hbh.ltd>
2021-03-11 16:01:55 +0800

    2021-03-11 227. 基本计算器 II

---
+++ src/lxl/y2021/MAR/BasicValculatorii.java
@@ -0,0 +1,87 @@
+package lxl.y2021.MAR;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * @author lxl
+ * @program leetcode-hz
+ * @description: 227. 基本计算器 II
+ * 给你一个字符串表达式 s ，请你实现一个基本计算器来计算并返回它的值。
+ * <p>
+ * 整数除法仅保留整数部分。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：s = "3+2*2"
+ * 输出：7
+ * <p>
+ * 示例 2：
+ * <p>
+ * 输入：s = " 3/2 "
+ * 输出：1
+ * <p>
+ * 示例 3：
+ * <p>
+ * 输入：s = " 3+5 / 2 "
+ * 输出：5
+ * <p>
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= s.length <= 3 * 105
+ * s 由整数和算符 ('+', '-', '*', '/') 组成，中间由一些空格隔开
+ * s 表示一个 有效表达式
+ * 表达式中的所有整数都是非负整数，且在范围 [0, 231 - 1] 内
+ * 题目数据保证答案是一个 32-bit 整数
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/basic-calculator-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @date 2021/3/10 9:54
+ * @Version 1.0
+ */
+public class BasicValculatorii {
+
+    public int calculate(String s) {
+        Deque<Integer> stack = new LinkedList<Integer>();
+        char preSign = '+';
+        int num = 0;
+        int n = s.length();
+        for (int i = 0; i < n; ++i) {
+            if (Character.isDigit(s.charAt(i))) {
+                num = num * 10 + s.charAt(i) - '0';
+            }
+            if (!Character.isDigit(s.charAt(i)) && s.charAt(i) != ' ' || i == n - 1) {
+                switch (preSign) {
+                    case '+':
+                        stack.push(num);
+                        break;
+                    case '-':
+                        stack.push(-num);
+                        break;
+                    case '*':
+                        stack.push(stack.pop() * num);
+                        break;
+                    default:
+                        stack.push(stack.pop() / num);
+                }
+                preSign = s.charAt(i);
+                num = 0;
+            }
+        }
+        int ans = 0;
+        while (!stack.isEmpty()) {
+            ans += stack.pop();
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        BasicValculatorii basicValculator = new BasicValculatorii();
+        System.out.println(basicValculator.calculate("1*2+4/2-3"));
+    }
+}
