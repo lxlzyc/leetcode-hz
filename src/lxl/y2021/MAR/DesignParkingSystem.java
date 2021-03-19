b812b76a7d95f35884ab12eea1f3bbb5c46cad0d
liuxiangli <liuxiangli@hbh.ltd>
2021-03-19 10:08:40 +0800

    2021-03-19 1603. 设计停车系统

---
+++ src/lxl/y2021/MAR/DesignParkingSystem.java
@@ -0,0 +1,63 @@
+package lxl.y2021.MAR;
+
+/**
+ * @author lxl
+ * @program leetcode-hz
+ * @description: 1603. 设计停车系统
+ * 请你给一个停车场设计一个停车系统。停车场总共有三种不同大小的车位：大，中和小，每种尺寸分别有固定数目的车位。
+ * <p>
+ * 请你实现 ParkingSystem 类：
+ * <p>
+ * ParkingSystem(int big, int medium, int small) 初始化 ParkingSystem 类，三个参数分别对应每种停车位的数目。
+ * bool addCar(int carType) 检查是否有 carType 对应的停车位。 carType 有三种类型：大，中，小，分别用数字 1， 2 和 3 表示。一辆车只能停在  carType 对应尺寸的停车位中。如果没有空车位，请返回 false ，否则将该车停入车位并返回 true 。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：
+ * ["ParkingSystem", "addCar", "addCar", "addCar", "addCar"]
+ * [[1, 1, 0], [1], [2], [3], [1]]
+ * 输出：
+ * [null, true, true, false, false]
+ * <p>
+ * 解释：
+ * ParkingSystem parkingSystem = new ParkingSystem(1, 1, 0);
+ * parkingSystem.addCar(1); // 返回 true ，因为有 1 个空的大车位
+ * parkingSystem.addCar(2); // 返回 true ，因为有 1 个空的中车位
+ * parkingSystem.addCar(3); // 返回 false ，因为没有空的小车位
+ * parkingSystem.addCar(1); // 返回 false ，因为没有空的大车位，唯一一个大车位已经被占据了
+ * <p>
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 0 <= big, medium, small <= 1000
+ * carType 取值为 1， 2 或 3
+ * 最多会调用 addCar 函数 1000 次
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/design-parking-system
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @date 2021/3/19 9:35
+ * @Version 1.0
+ */
+public class DesignParkingSystem {
+
+    private int[] help;
+
+    public DesignParkingSystem(int big, int medium, int small) {
+        help = new int[4];
+        help[1] = big;
+        help[2] = medium;
+        help[3] = small;
+    }
+
+    public boolean addCar(int carType) {
+        if (help[carType] > 0) {
+            help[carType]--;
+            return true;
+        }
+        return false;
+    }
+}
